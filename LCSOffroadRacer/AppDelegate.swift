c3baa4b71dfb1662188f6caee67985eb23f94523
Tom Shiflet <tom.shiflet@mylcs.com>
2021-01-19 11:56:27 -0600

    Initial Commit

---
+++ LCSOffroadRacer/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  LCSOffroadRacer
+//
+//  Created by Tom Shiflet on 1/19/21.
+//  Copyright © 2021 Tom Shiflet. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
