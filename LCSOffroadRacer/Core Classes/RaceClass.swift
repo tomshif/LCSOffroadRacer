a178e8ed5a3fa3ca3d8f0d5d6e41674d1ae0dd8b
Levi Lobbes <levi.lobbes24@mylcs.com>
2021-01-25 08:49:08 -0600

    Race Class setup

--- LCSOffroadRacer/Core Classes/RaceClass.swift
+++ LCSOffroadRacer/Core Classes/RaceClass.swift
@@ -11,17 +11,90 @@ import SpriteKit
 class RaceClass
 {
     
-
+    var currentRank:Int = 1
+    
+    var isRacing:Bool = true
+    
+    var start = CGPoint()
+    var finish = CGPoint()
+    
+    var easyCarAmount:Int = 3
+    var medCarAmount:Int = 5
+    var hardCarAmount:Int = 8
+    
+    var isNight:Bool = false
+    var isEvening:Bool = false
+    var isMorning:Bool = false
+    var isDay:Bool = true
+    
+    var isRaining:Bool = false
+    var isSnowing:Bool = false
+    var isStorming:Bool = false
+    var isSunny:Bool = true
+    
+    var timeRacing:Int = 0
+    
+    var isHard:Bool = false
+    var isMedium:Bool = false
+    var isEasy:Bool = true
     // variables
 
-
-
+    let hardFirstPlaceMoney:Int = 1000
+    let hardSecondPlaceMoney:Int = 750
+    let hardThirdPlaceMoney:Int = 500
+    let hardParticipationMoney:Int = 100
+    let hardLastPlaceMoney:Int = 50
+    let medFirstPlaceMoney:Int = 500
+    let medSecondPlaceMoney:Int = 350
+    let medThirdPlaceMoney:Int = 250
+    let medFourthPlaceMoney:Int = 100
+    let medLastPlaceMoney:Int = 25
+    let easyFirstPlaceMoney:Int = 100
+    let easySecondPlaceMoney:Int = 50
+    let easyThirdPlaceMoney:Int = 5
     // constants
 
+    func createMap()
+    {
+        func createCheckpoints()
+        {
+            
+        } // func createCheckpoints
+    } // func createMap
+    
+    func raceStats()
+    {
+        
+    } // func raceStats
+    
+    func destroyCar()
+    {
+        
+    } // func destroyCar
+    
+    func raceRewards()
+    {
+        
+    } // func raceRewards
+    
+    func difficulty()
+    {
+        
+    } // func difficulty
+    
+    func timeOfDay()
+    {
+        
+    } // func timeOfDay
+    
+    func weather()
+    {
+        
+    } // func weather
 
     init()
     {
         
-    }
+    } // init
 
-}
+} // RaceClass
