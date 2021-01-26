9615cb9f3797752ca34901387bffbf9a03b0ca89
wade garrison <wade.garrison24@mylcs.com>
2021-01-26 08:19:23 -0600

    HUDClass Setup

--- LCSOffroadRacer/Core Classes/HUDClass.swift
+++ LCSOffroadRacer/Core Classes/HUDClass.swift
@@ -12,14 +12,35 @@ import SpriteKit
 class HUDClass
 {
     // variables
-
-
-
+    var speedometer = SKSpriteNode(imageNamed: "speedometer")
+    var directionalArrow = SKSpriteNode(imageNamed: "arrow")
+    var countdown1 = SKSpriteNode(imageNamed: "countdown1")
+    var countdown2 = SKSpriteNode(imageNamed:"countdown2")
+    var countdown3 = SKSpriteNode(imageNamed: "countdown3")
+    var countdownGo = SKSpriteNode(imageNamed: "go")
+    var currentTimer = SKLabelNode(fontNamed: "Arial")
+    
+    // intend to add a variable that shows the player's place; not sure whether it should be separate variables for each place or just one.
+    // no idea what else to add for variables.
+    
     // constants
 
-
+    
+    
+    func raceStart()
+    {
+        
+    } // race start
+    // probably will just cover the interface showing up, as well as the race countdown starting.
+    
+func updateAll()
+{
+    
+} // updateAll
+    
     init()
     {
         
-    }
-}
+    } // Initializer
+    
+} // HUDClass
