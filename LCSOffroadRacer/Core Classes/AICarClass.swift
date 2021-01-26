82ebbec55b402f82ebbcce3b5c26382c1cd753d7
Aaron Park <AaronJ.Park0304@gmail.com>
2021-01-26 08:21:07 -0600

    Initial Variables

--- LCSOffroadRacer/Core Classes/AICarClass.swift
+++ LCSOffroadRacer/Core Classes/AICarClass.swift
@@ -11,11 +11,15 @@ import SpriteKit
 class AICarClass
 {
     // variables
-
-
+    var AICarSpeed:Double = 0
+    var AICarEnginePower:Double = 0
+    var AICarTraction:Double = 0
+    var AICarTurnRate:Double = 0
+    // var AICarVector:CGVector = vector2(0, 0)
 
     // constants
-
+    let maxAICarSpeed:Double = 300
+    let maxAICarEnginePower:Double = 300
 
     init()
     {
