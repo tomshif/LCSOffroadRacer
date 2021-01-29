4628f650711f4a22aa6b0bb66e12354d44b857b6
Jun <jun.lee23@mylcs.com>
2021-01-29 08:46:36 -0600

    mapClass changes

--- LCSOffroadRacer/Core Classes/MapClass.swift
+++ LCSOffroadRacer/Core Classes/MapClass.swift
@@ -12,56 +12,58 @@ import GameplayKit
 class MapClass
 {
     // variables
+    var theScene:GameScene?
+    let tileWidth:CGFloat = 128
     var mapTex=SKSpriteNode()
-    var colorOffset:CGFloat=0.0
     var topLayer=SKTileMapNode()
-    
+    let map = SKNode()
+   
     
     // constants
-    let map = SKNode()
-    let tileSet = 0
-    let tileSize = CGSize(width: 128, height: 128)
-    let columns = 128//n/a
-    let rows = 128//n/a
+    
+    
+    
+    
+    init(scene: GameScene)
+    {
+        theScene = scene
+        
+        let tileSize = CGSize(width: 128, height: 128)
+        let columns = 128
+        let rows = 128
+        
+        
+        func mapPosition()
+        {
+            var y = map.position.y
+            var x = map.position.x
+            
+            for y in 0...127
+            {
+                for x in 0...127
+                {
+                    var posX:CGFloat = 0
+                    var posY:CGFloat = 0
 
+                    posX = -64 * tileWidth + tileWidth * CGFloat(x)
+                    posY = -64 * tileWidth + tileWidth * CGFloat(y)
+                    let temptile=SKSpriteNode(imageNamed: "grass01")
+                    temptile.position=CGPoint(x: posX, y: posY)
+                    theScene!.irAnchor.addChild(temptile)
+                }//for x
+            }//for y
+        }//mapPosition()
+
+            mapPosition()
+    }//init
+
+    
+    
     init()
     {
-        let titleSet = SKTileSet(named: "")
-        
-        func makeNoiseMap(columns: Int, rows: Int) -> GKNoiseMap {
-            
-            let seed=Int32(random(min: 1, max: 50000))
-            //let source = GKBillowNoiseSource(frequency: 0.015, octaveCount: 2, persistence: 0.2, lacunarity: 0.005, seed: seed)
-            let source=GKPerlinNoiseSource()
-            source.persistence = 0.8
-            
-            let noise = GKNoise(source)
-            let sizeRange=random(min: 3.5, max: 6.0)
-            let size = vector2(Double(sizeRange), Double(sizeRange))
-            let origin = vector2(0.0, 0.0)
-            let sampleCount = vector2(Int32(columns), Int32(rows))
-            
-            return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
-        }
-        
-        let noiseMap = makeNoiseMap(columns: columns, rows: rows)
-        
-          for column in 0 ..< columns {
-                  for row in 0 ..< rows {
-                      let location = vector2(Int32(row), Int32(column))
-                      let terrainHeight = noiseMap.value(at: location)
-                     
-//                      if terrainHeight < -0.5 {
-//                          topLayer.setTileGroup(deadGrass, forColumn: column, row: row)
-//                      } else if terrainHeight < 0.5{
-//                          topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
-//                      }
-//                      else
-//                      {
-//                          topLayer.setTileGroup(grass01Tiles, forColumn: column, row: row)
-//                      }
-                  }
-              }
+      
+                  
+              
 
         
     
