ecb157fcc531493e785e6d2fc50647731aa49e91
Tom Shiflet <tom.shiflet@mylcs.com>
2021-02-01 08:50:51 -0600

    partclass setup

--- LCSOffroadRacer/Core Classes/PartClass.swift
+++ LCSOffroadRacer/Core Classes/PartClass.swift
@@ -12,17 +12,18 @@ class PartClass
 {
     // variables
     
-var theScene:GameScene?
+    var theScene:GameScene?
 
-    var CHASSIS:Int = 0
-    var ENGINE:Int = 1
-    var TIRES:Int = 2
+    var type:Int = -1
+    var quality:Int = -1
     
-    var Speed:CGFloat=0
-    var Tretion:CGFloat=0
-    var Accel:CGFloat=0
-    var TurnRate:CGFloat=0
     
+    var speed:CGFloat=0
+    var traction:CGFloat=0
+    var accel:CGFloat=0
+    var turnRate:CGFloat=0
+    
+    var sprite=SKSpriteNode()
     
     
 
@@ -33,6 +34,57 @@ var theScene:GameScene?
     {
         
     }
+    
+    init(scene:GameScene)
+    {
+        theScene=scene
+        type = Int(random(min: 0, max: 4.999999999999))
+        quality = Int(random(min: 0, max: 4.999999999999))
+        createStats()
+    }
 
+    init(scene:GameScene, partType:Int)
+    {
+        theScene=scene
+        type=partType
+        quality = Int(random(min: 0, max: 4.999999999999))
+        createStats()
+    }
+    
+    init(scene:GameScene, partType:Int, qual:Int)
+    {
+        theScene=scene
+        type=partType
+        quality=qual
+        createStats()
+        
+    }
+    
+    func createStats()
+    {
+        switch type
+        {
+        
+        case PARTTYPE.ENGINE:
+            speed = (CGFloat(quality)/5)+0.2
+            
+        case PARTTYPE.AXLE:
+            turnRate=(CGFloat(quality)/5)+0.2
+            
+        case PARTTYPE.TIRES:
+            traction=(CGFloat(quality)/5)+0.2
+            
+        case PARTTYPE.GEARBOX:
+            accel=(CGFloat(quality)/5)+0.2
+            
+        case PARTTYPE.CHASSIS:
+            break
+            
+        default:
+            print("Error - Invalid part type in PartClass.createStats().")
+            
+        } // switch
+    } // createStats()
+    
 } // class PartClass
 
