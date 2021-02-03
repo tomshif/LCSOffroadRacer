011b7989aa4abab8b0748c99ad67a0881e907876
Jun <jun.lee23@mylcs.com>
2021-02-03 08:38:56 -0600

    mapClass update // four different tiles depending on height

--- LCSOffroadRacer/Core Classes/MapClass.swift
+++ LCSOffroadRacer/Core Classes/MapClass.swift
@@ -28,10 +28,20 @@ class MapClass
     {
         theScene = scene
         
+        theScene!.irAnchor.addChild(map)
+        
+        let tileSet = SKTileSet(named: "Grass Map")!
         let tileSize = CGSize(width: 128, height: 128)
         let columns = 128
         let rows = 128
-        
+        let grassTiles = tileSet.tileGroups.first { $0.name == "grass01"}
+        let sandTiles = tileSet.tileGroups.first { $0.name == "sand"}
+        let dirtTiles = tileSet.tileGroups.first { $0.name == "dirt"}
+        let waterTiles = tileSet.tileGroups.first { $0.name == "water"}
+        let bottomLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+        bottomLayer.fill(with: waterTiles)
+        map.addChild(bottomLayer)
+
         
         func mapPosition()
         {
@@ -47,17 +57,71 @@ class MapClass
 
                     posX = -64 * tileWidth + tileWidth * CGFloat(x)
                     posY = -64 * tileWidth + tileWidth * CGFloat(y)
-                    let temptile=SKSpriteNode(imageNamed: "grass01")
-                    temptile.position=CGPoint(x: posX, y: posY)
-                    temptile.zPosition = -100
-                    theScene!.irAnchor.addChild(temptile)
+//                    let temptile=SKSpriteNode(imageNamed: "grass01")
+//                    temptile.position=CGPoint(x: posX, y: posY)
+//                    temptile.zPosition = -100
+//                    theScene!.irAnchor.addChild(temptile)
                 }//for x
              }//for y
         }//mapPosition()
 
             mapPosition()
-    }//init
+         
+         topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+         
 
+         // make SpriteKit do the work of placing specific tiles
+         topLayer.enableAutomapping = true
+         
+         // add the grass/water layer to our main map node
+         map.addChild(topLayer)
+         
+        
+          func makeNoiseMap(columns: Int, rows: Int) -> GKNoiseMap {
+              
+              let seed=Int32(random(min: 1, max: 50000))
+              //let source = GKBillowNoiseSource(frequency: 0.015, octaveCount: 2, persistence: 0.2, lacunarity: 0.005, seed: seed)
+              let source=GKPerlinNoiseSource()
+              source.persistence = 0.8
+              
+              let noise = GKNoise(source)
+              let sizeRange=random(min: 3.5, max: 6.0)
+              let size = vector2(Double(sizeRange), Double(sizeRange))
+              let origin = vector2(0.0, 0.0)
+              let sampleCount = vector2(Int32(columns), Int32(rows))
+              
+              return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+          }
+          
+          let noiseMap = makeNoiseMap(columns: columns, rows: rows)
+          let mapT=SKTexture(noiseMap: noiseMap)
+          mapTex=SKSpriteNode(texture: mapT)
+
+        
+         // create the noise map
+         
+         for column in 0 ..< columns {
+             for row in 0 ..< rows {
+                 let location = vector2(Int32(row), Int32(column))
+                 let terrainHeight = noiseMap.value(at: location)
+                
+                 if terrainHeight > -0.5 && terrainHeight < -0.2 {
+                     topLayer.setTileGroup(dirtTiles, forColumn: column, row: row)
+                 } else if terrainHeight < 0.5{
+                     topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
+                 }
+                 else if terrainHeight < 1 && terrainHeight > 0.5{
+                     topLayer.setTileGroup(sandTiles, forColumn: column, row: row)
+                 }
+             }
+         }
+         // return animated tiles in a single layer
+        
+                
+        
+    }//init
+    
+        
     
     
     init()
