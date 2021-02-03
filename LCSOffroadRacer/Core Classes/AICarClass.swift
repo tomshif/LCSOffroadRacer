e2d4639e1ce1ec0d04e89b9bbf7e507ddd89d91e
Aaron Park <AaronJ.Park0304@gmail.com>
2021-02-03 08:56:46 -0600

    Hey

--- LCSOffroadRacer/Core Classes/AICarClass.swift
+++ LCSOffroadRacer/Core Classes/AICarClass.swift
@@ -14,11 +14,11 @@ class AICarClass
     var AICarSprite = SKSpriteNode(imageNamed: "car_blue_1")
     var theScene:GameScene?
     
-    var AICarSpeed:Double = 0
-    var AICarEnginePower:Double = 0
-    var AICarTraction:Double = 0
-    var AICarTurnRate:Double = 0
-    var AICarAcceleration:Double = 0
+    var AICarSpeed:CGFloat = 0
+    var AICarEnginePower:CGFloat = 0
+    var AICarTraction:CGFloat = 0
+    var AICarTurnRate:CGFloat = 0
+    var AICarAcceleration:CGFloat = 0
     var AICarVector = CGVector()
     
     var isAccelearting:Bool = false
@@ -33,11 +33,64 @@ class AICarClass
     init(scene:GameScene)
     {
         theScene = scene
+        theScene!.irAnchor.addChild(AICarSprite)
     } // init
+    
+    func generateAICarStats()
+    {
+        AICarSpeed = random(min: 10, max: 50)
+        AICarEnginePower = random(min: 10, max: 30)
+        AICarTraction = random(min: 10, max: 20)
+        AICarTurnRate = random(min: 10, max: 30)
+        AICarAcceleration = random(min: 15, max: 60)
+    }
     
     func goToFinishLine()
     {
+        // let dx = theScene!.AICrSprite.position.x
+        // let dy = theScene!.AICarSprite.position.y
         
+        /*
+        var angleToPlyaer = atan2(dy, dx)
+        
+        if (angleToPlyaer < 0)
+        {
+            angleToPlyaer += CGFloat.pi*2
+        }
+        
+        if (sprite.zRotation < 0)
+        {
+            sprite.zRotation += CGFloat.pi*2
+        }
+        
+        if (sprite.zRotation > CGFloat.pi*2)
+        {
+            sprite.zRotation -= CGFloat.pi*2
+        }
+        
+        var angleDiff = sprite.zRotation - angleToPlyaer
+        
+        if (angleDiff < 0)
+        {
+            angleDiff += CGFloat.pi*2
+        }
+        
+        if (angleDiff > CGFloat.pi)
+        {
+            sprite.zRotation += turnRate
+        }
+        
+        else
+        {
+            sprite.zRotation -= turnRate
+        }
+        
+        let moveDX = cos(sprite.zRotation)*speed
+        let moveDY = sin(sprite.zRotation)*speed
+        sprite.position.x += moveDX
+        sprite.position.y += moveDY
+        
+        */
     } // func goToFinishLine
     
     func slowDown()
@@ -69,6 +122,11 @@ class AICarClass
     {
         
     } // func reverse
+    
+    func didMove()
+    {
+        
+    } // func didMove
     
     func update()
     {
