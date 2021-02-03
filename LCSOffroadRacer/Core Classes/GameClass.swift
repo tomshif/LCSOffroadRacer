f6223abd16c84b02843f874d571477dfce72e36c
Tom Shiflet <tom.shiflet@mylcs.com>
2021-02-03 09:05:52 -0600

    Read names lists

--- LCSOffroadRacer/Core Classes/GameClass.swift
+++ LCSOffroadRacer/Core Classes/GameClass.swift
@@ -13,10 +13,54 @@ class GameClass
     // variables
     var AICarList=[AICarClass]()
     
+    var firstNameList=[String]()
+    var lastNameList=[String]()
+    var locationList=[String]()
+    
 
     // constants
 
 
+    
+    func loadNameLists() -> String!
+    {
+        guard let filepath = Bundle.main.path(forResource: "AchievementList", ofType: "txt")
+        else
+        {
+            print("Error opening file")
+            return nil
+        }
+        
+        let file:FileHandle?=FileHandle(forReadingAtPath: filepath)
+        let data=file?.readDataToEndOfFile()
+        
+        file?.closeFile()
+        
+        let dataString=String(data: data!, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))
+        
+        //print(dataString!)
+        
+        let lines = dataString!.split(separator: "\n")
+        
+        var lineNum:Int=0
+        for line in lines
+        {
+            let columns = line.split(separator: ",")
+            
+            for col in columns
+            {
+                locationList.append(String(col))
+            }
+            
+        } // for each line
+        print(locationList)
+
+        
+        
+        
+        return nil
+            
+    } // loadAchievements
     init()
     {
         
