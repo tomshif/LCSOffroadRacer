cf0f61c7237d62db67cffb6c837a30ef98942730
Aaron Park <AaronJ.Park0304@gmail.com>
2021-02-10 08:26:18 -0600

    changesssss

--- LCSOffroadRacer/Core Classes/MapClass.swift
+++ LCSOffroadRacer/Core Classes/MapClass.swift
@@ -18,17 +18,12 @@ class MapClass
     var topLayer=SKTileMapNode()
     var treeLayer = SKTileMapNode()
     let map = SKNode()
-   
-    
     // constants
-    
-    
     
     
     init(scene: GameScene)
     {
         theScene = scene
-        
         theScene!.irAnchor.addChild(map)
         
         let tileSet = SKTileSet(named: "Grass Map")!
@@ -58,8 +53,8 @@ class MapClass
         
         func mapPosition()
         {
-            var y = map.position.y
             var x = map.position.x
+            var y = map.position.y
             
             for y in 0...127
             {
@@ -70,81 +65,85 @@ class MapClass
 
                     posX = -64 * tileWidth + tileWidth * CGFloat(x)
                     posY = -64 * tileWidth + tileWidth * CGFloat(y)
-//                    let temptile=SKSpriteNode(imageNamed: "grass01")
-//                    temptile.position=CGPoint(x: posX, y: posY)
-//                    temptile.zPosition = -100
-//                    theScene!.irAnchor.addChild(temptile)
-                }//for x
-             }//for y
-        }//mapPosition()
+                    /*
+                    let temptile=SKSpriteNode(imageNamed: "grass01")
+                    temptile.position=CGPoint(x: posX, y: posY)
+                    temptile.zPosition = -100
+                    theScene!.irAnchor.addChild(temptile)
+                    */
+                } //for
+             } //for
+        } // func mapPosition
 
-            mapPosition()
-         
-         topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
-         topLayer.enableAutomapping = true
-         map.addChild(topLayer)
-         
+        mapPosition()
         
-        //create the noisemap
+        topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+        topLayer.enableAutomapping = true
+        map.addChild(topLayer)
+        // creates the noisemap
         
-          func makeNoiseMap(columns: Int, rows: Int) -> GKNoiseMap {
+        func makeNoiseMap1(columns: Int, rows: Int) -> GKNoiseMap
+        {
+            let seed = Int32(random(min: 1, max: 50000))
+            //let source = GKBillowNoiseSource(frequency: 0.015, octaveCount: 2, persistence: 0.2, lacunarity: 0.005, seed: seed)
+            let source=GKPerlinNoiseSource()
+            source.persistence = 0.8
               
-              let seed=Int32(random(min: 1, max: 50000))
-              //let source = GKBillowNoiseSource(frequency: 0.015, octaveCount: 2, persistence: 0.2, lacunarity: 0.005, seed: seed)
-              let source=GKPerlinNoiseSource()
-              source.persistence = 0.8
+            let noise = GKNoise(source)
+            let sizeRange=random(min: 3.5, max: 6.0)
+            let size = vector2(Double(sizeRange), Double(sizeRange))
+            let origin = vector2(0.0, 0.0)
+            let sampleCount = vector2(Int32(columns), Int32(rows))
               
-              let noise = GKNoise(source)
-              let sizeRange=random(min: 3.5, max: 6.0)
-              let size = vector2(Double(sizeRange), Double(sizeRange))
-              let origin = vector2(0.0, 0.0)
-              let sampleCount = vector2(Int32(columns), Int32(rows))
-              
-              return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
-          }//noiseMap
+            return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+        } // func makeNoiseMap
           
-          let noiseMap = makeNoiseMap(columns: columns, rows: rows)
-          let mapT=SKTexture(noiseMap: noiseMap)
-          mapTex=SKSpriteNode(texture: mapT)
-
-         
-        //different tiles depending on terrain height
+        let noiseMap1 = makeNoiseMap1(columns: columns, rows: rows)
+        let mapT1 = SKTexture(noiseMap: noiseMap1)
+        mapText1 = SKSpriteNode(texture: mapT1)
+        // Generates different tiles depending on terrain height
         
-         for column in 0 ..< columns
-         {
-             for row in 0 ..< rows
-             {
-                 let location = vector2(Int32(row), Int32(column))
-                 let terrainHeight = noiseMap.value(at: location)
+        func makeNoiseMap2(columns: Int, rows: Int) -> GKNoiseMap
+        {
+            let seed = Int32(random(min: 1, max: 50000))
+            //let source = GKBillowNoiseSource(frequency: 0.015, octaveCount: 2, persistence: 0.2, lacunarity: 0.005, seed: seed)
+            let source=GKPerlinNoiseSource()
+            source.persistence = 0.8
+              
+            let noise = GKNoise(source)
+            let sizeRange=random(min: 3.5, max: 6.0)
+            let size = vector2(Double(sizeRange), Double(sizeRange))
+            let origin = vector2(0.0, 0.0)
+            let sampleCount = vector2(Int32(columns), Int32(rows))
+              
+            return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+        } // func makeNoiseMap
+          
+        let noiseMap2 = makeNoiseMap2(columns: columns, rows: rows)
+        let mapT2 = SKTexture(noiseMap: noiseMap2)
+        mapText2 = SKSpriteNode(texture: mapT2)
+        
+        for column in 0 ..< columns
+        {
+            for row in 0 ..< rows
+            {
+                let location = vector2(Int32(row), Int32(column))
+                let terrainHeight = noiseMap2.value(at: location)
                 
-                 if terrainHeight > -0.5 && terrainHeight < -0.2
-                 {
-                     topLayer.setTileGroup(dirtTiles, forColumn: column, row: row)
-
-                 } else if terrainHeight < 0.5{
-                     topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
-                 }
-                 else if terrainHeight < 1 && terrainHeight > 0.5{
-                     topLayer.setTileGroup(sandTiles, forColumn: column, row: row)
-                 }
-               
-             }//for rows
-         }//for columns
-        
-        
+                if (terrainHeight > -0.5 && terrainHeight < -0.2)
+                {
+                    topLayer.setTileGroup(dirtTiles, forColumn: column, row: row)
+                } // if
                 
-        
-        
-    }//init
-    
-        
-    
-    
-    init()
-    {
-      // This initializer should never be used
-                  
-    
-    }
-
-}
+                else if (terrainHeight < 0.5)
+                {
+                    topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
+                } // else if
+                else if (terrainHeight < 1 && terrainHeight > 0.5)
+                {
+                    topLayer.setTileGroup(sandTiles, forColumn: column, row: row)
+                } // else if
+            } // for
+        } // for
+    } // init
+} // class MapClass
